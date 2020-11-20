32b2735b599a5fa46f41907fc881021de5ba7abf
Tanushree Patil <63952667+TanushriPatil@users.noreply.github.com>
2020-11-20 23:50:31 +0530

    Initial commit

---
+++ teproject/app/src/main/java/com/example/teproject/Register.java
@@ -0,0 +1,96 @@
+package com.example.teproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Register extends AppCompatActivity {
+
+    EditText mName, mEmail, mPassword, mMobile;
+    Button mSignUpBtn, mLoginBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mName = findViewById(R.id.name);
+        mEmail = findViewById(R.id.email);
+        mMobile = findViewById(R.id.mobileNumber);
+        mPassword = findViewById(R.id.password);
+        mLoginBtn = findViewById(R.id.rloginbutn);
+        mSignUpBtn = findViewById(R.id.rsignupbtn);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+            }
+        });
+
+        mSignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String pass = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is required.");
+                    return;
+                }
+                if(TextUtils.isEmpty(pass)){
+                    mPassword.setError("Password is required.");
+                    return;
+                }
+                if(pass.length() < 5){
+                    mPassword.setError("Password must be >= 5 characters");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                // register user in firebase
+                fAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Register.this, "Account created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }else{
+                            Toast.makeText(Register.this, "Error..! " + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                String name = mName.getText().toString();
+
+            }
+        });
+    }
+
+
+}
