a36f86a3adf7fe2fbd8360fc035decd288839d90
Tanushree Patil <63952667+TanushriPatil@users.noreply.github.com>
2020-11-23 19:31:06 +0530

    Firestore added

--- teproject/app/src/main/java/com/example/teproject/Register.java
+++ teproject/app/src/main/java/com/example/teproject/Register.java
@@ -6,10 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -17,29 +19,44 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Calendar;
 
 public class Register extends AppCompatActivity {
 
-    EditText mName, mEmail, mPassword, mMobile;
+    EditText mName, mEmail, mPassword, mBranch, mRegistrationID, mRollno;
     Button mSignUpBtn, mLoginBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
     RadioGroup radioGroup;
+    RadioButton radioButton;
+
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+
         mName = findViewById(R.id.name);
         mEmail = findViewById(R.id.email);
-        mMobile = findViewById(R.id.mobileNumber);
+        mBranch = findViewById(R.id.branch);
         mPassword = findViewById(R.id.password);
         mLoginBtn = findViewById(R.id.rloginbutn);
         mSignUpBtn = findViewById(R.id.rsignupbtn);
+        mRegistrationID = findViewById(R.id.registrationID);
+        mRollno = findViewById(R.id.rollnostudent);
+        radioGroup = findViewById(R.id.rdgroup);
+        progressBar = findViewById(R.id.progressBar);
 
         fAuth = FirebaseAuth.getInstance();
-        progressBar = findViewById(R.id.progressBar);
+        fStore = FirebaseFirestore.getInstance();
+
 
         if(fAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
@@ -54,13 +71,20 @@ public class Register extends AppCompatActivity {
             }
         });
 
-        radioGroup = findViewById(R.id.rdgroup);
-
         mSignUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = mEmail.getText().toString().trim();
                 String pass = mPassword.getText().toString().trim();
+                String name = mName.getText().toString();
+                String regID = mRegistrationID.getText().toString();
+                String branch = mBranch.getText().toString();
+                int radio = radioGroup.getCheckedRadioButtonId();
+
+                radioButton = findViewById(radio);
+
+                Log.d("radio", "radio button checked is: "+ radio);
+
 
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Email is required.");
@@ -83,6 +107,7 @@ public class Register extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(Register.this, "Account created", Toast.LENGTH_SHORT).show();
+                            DocumentReference documentReference = fStore.document("year/"+year+"- "+(year+1)+"/Users/Users");
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         }else{
                             Toast.makeText(Register.this, "Error..! " + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
@@ -90,10 +115,13 @@ public class Register extends AppCompatActivity {
                     }
                 });
 
-                String name = mName.getText().toString();
+
 
             }
         });
+
+
+
     }
 
 
