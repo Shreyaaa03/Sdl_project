90b4801bb6d88a807c63c27d44b6fe6bc6013de7
Tanushree Patil <63952667+TanushriPatil@users.noreply.github.com>
2020-11-24 20:45:32 +0530

    user added to firestore

--- teproject/app/src/main/java/com/example/teproject/Register.java
+++ teproject/app/src/main/java/com/example/teproject/Register.java
@@ -1,51 +1,97 @@
 package com.example.teproject;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewOutlineProvider;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
-    EditText mName, mEmail, mPassword, mMobile;
+    EditText mName, mEmail, mPassword, mBranch,mRegistrationID, mRollno;
     Button mSignUpBtn, mLoginBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
     RadioGroup radioGroup;
+    RadioButton radioButton;
+    //  Spinner mBranch;
+    String rollno;
+    Boolean role;
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+
+
         mName = findViewById(R.id.name);
         mEmail = findViewById(R.id.email);
-        mMobile = findViewById(R.id.mobileNumber);
         mPassword = findViewById(R.id.password);
         mLoginBtn = findViewById(R.id.rloginbutn);
         mSignUpBtn = findViewById(R.id.rsignupbtn);
+        mRegistrationID = findViewById(R.id.registrationID);
+        mRollno = findViewById(R.id.rollnostudent);
+        radioGroup = findViewById(R.id.rdgroup);
+        progressBar = findViewById(R.id.progressBar);
+        mBranch = findViewById(R.id.branchspinner);
 
         fAuth = FirebaseAuth.getInstance();
-        progressBar = findViewById(R.id.progressBar);
+        fStore = FirebaseFirestore.getInstance();
 
         if(fAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
             finish();
         }
 
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Branches, android.R.layout.simple_spinner_item);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        mBranch.setAdapter(adapter);
+//        mBranch.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                radioButton = findViewById(checkedId);
+                String roletext = radioButton.getText().toString();
+                if (roletext.equals("Student")) {
+                    mRollno.setVisibility(View.VISIBLE);
+                    role = true;
+
+
+                } else if(roletext.equals("Teacher")){
+                    mRollno.setVisibility(View.INVISIBLE);
+                    role = false;
+                }
+            }
+        });
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,13 +100,19 @@ public class Register extends AppCompatActivity {
             }
         });
 
-        radioGroup = findViewById(R.id.rdgroup);
-
         mSignUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = mEmail.getText().toString().trim();
                 String pass = mPassword.getText().toString().trim();
+                String name = mName.getText().toString();
+                String regID = mRegistrationID.getText().toString();
+                String branch = mBranch.getText().toString();
+
+                if(role){
+                    rollno = mRollno.getText().toString();
+                }
+                //     String branch = mBranch.getItemAtPosition(i).toString();
 
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Email is required.");
@@ -82,19 +134,52 @@ public class Register extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
+                            // AUTHENTICATION SUCCESSFUL
                             Toast.makeText(Register.this, "Account created", Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+                            // ADD DATA TO FIRESTORE
+                            DocumentReference docR = fStore.document("year/"+year+"- "+(year+1)+"/Users/"+regID);
+                            //     DocumentReference docref = fStore.collection("Year").document(year+"-"+(year+1)).collection("Users").document(regID);
+                            //     DocumentReference docr = fStore.collection("Users").document(regID);
+                            Map<String, Object> datatosave = new HashMap<>();
+
+                            datatosave.put("Name", name);
+                            if(role){
+                                datatosave.put("Role", true);
+                                datatosave.put("RollNo", rollno);
+                            } else{
+                                datatosave.put("Role", false);
+                            }
+                            datatosave.put("Email", email);
+                            datatosave.put("Branch", branch);
+                            datatosave.put("Password", pass);
+
+
+                            datatosave.put("RegistrationID", regID);
+                            docR.set(datatosave).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Log.d("Roll", "rollno: "+ rollno);
+                                        Log.d("Hooray", "Task successful");
+                                    } else{
+                                        Log.d("Error", "There was an error!");
+                                    }
+                                }
+                            });
                         }else{
+                            // AUTHENTICATION UNSUCCESSFUL
                             Toast.makeText(Register.this, "Error..! " + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
-
-                String name = mName.getText().toString();
-
             }
         });
+
     }
+
+
 
 
 }
