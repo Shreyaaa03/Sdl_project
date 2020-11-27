ab3ef6298b1d8645fd57313f4e086d1fe4473e92
Shreya <Shreyaaa03@users.noreply.github.com>
2020-11-27 15:03:39 +0400

    + activity_group_profile.xml

--- teproject/app/src/main/java/com/example/teproject/GroupProfile.java
+++ teproject/app/src/main/java/com/example/teproject/GroupProfile.java
@@ -1,14 +1,31 @@
 package com.example.teproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 
 public class GroupProfile extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_profile);
+
+        textView = findViewById(R.id.tech_stack_id);
+
+        textView.setText("Whatever...");
+
+
     }
 }
