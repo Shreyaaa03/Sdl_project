1ecc2f3a52074942a0b19189b2aa673643d4c6f3
pratt3000 <pratt3000@gmail.com>
2020-11-29 17:21:25 +0530

    profile edit 2/3

--- teproject/app/src/main/java/com/example/teproject/editprofile.java
+++ teproject/app/src/main/java/com/example/teproject/editprofile.java
@@ -1,14 +1,90 @@
 package com.example.teproject;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
 public class editprofile extends AppCompatActivity {
+
+    TextView email, phone, linkedin, github, resume;
+    Button savechanges;
+
+    FirebaseFirestore fStore;
+    FirebaseAuth fAuth;
+
+    String RegID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.editprofile);
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+
+        email = findViewById(R.id.edittext1);
+        phone = findViewById(R.id.edittext2);
+        linkedin = findViewById(R.id.edittext3);
+        github = findViewById(R.id.edittext4);
+        resume = findViewById(R.id.edittext5);
+
+        savechanges = findViewById(R.id.savechanges);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        DocumentReference docR = fStore.document("year/"+year+"- "+(year+1)+"/IDS/"+fAuth.getCurrentUser().getUid());
+
+        docR.get().addOnSuccessListener(this, new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists()){
+                    RegID = documentSnapshot.getString("RegID");
+                    tp(year);
+
+                }
+
+            }
+        });
+
+        savechanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+    void tp(int year){
+        DocumentReference docR_2 = fStore.document("year/"+year+"- "+(year+1)+"/Users/"+RegID);
+        docR_2.get().addOnSuccessListener(this, new OnSuccessListener<DocumentSnapshot>() {
+
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists()) {
+                    email.setText(documentSnapshot.getString("Email"));
+                    phone.setText(documentSnapshot.getString("PhoneNo"));
+                    linkedin.setText(documentSnapshot.getString("Linkedin"));
+                    github.setText(documentSnapshot.getString("Github"));
+                    resume.setText(documentSnapshot.getString("Resume"));
+                }
+            }
+        });
+    }
+
 }
